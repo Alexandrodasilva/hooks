7566505a5dff7a86b7066e22fa9d1a2feed45b32
alexandrodasilva <alexsamdrosilva13@gmail.com>
2023-03-10 12:52:44 -0300

    UseCallback and its propretios

--- src/views/examples/UseCallback.jsx
+++ src/views/examples/UseCallback.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 
 const UseCallback = (props) => {
+    const [count, setCount] = useState(0);
+
+    const inc = delta =>{
+        setCount(count + delta)
+    }
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn"
+                    onClick={ _ => inc(6)}
+                    >+6</button>
+                    <button className="btn"
+                    onClick={ _ => inc(6)}
+                    >+12</button>
+                    <button className="btn"
+                    onClick={ _ => inc(6)}
+                    >+18</button>
+                </div>
+            </div>
         </div>
     )
 }
